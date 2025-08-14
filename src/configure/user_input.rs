1ad59c7aa290ec5e2517acf4855d5a8cd0b550f8
Michael Descy <mike@mjdescy.me>
2025-08-14 00:41:10 -0400

    Refactor user input handling to use rustyline
    - `rustyline` improves user input handling.
    - For example, it allows arrow keys to be used to navigate input history.
    
    Fixed compiler warnings after merge

--- src/configure/user_input.rs
+++ src/configure/user_input.rs
@@ -1,14 +1,13 @@
-use std::io;
-use std::io::Write;
-
+use rustyline::DefaultEditor;
 
 pub fn get_user_input(prompt: &str) -> String {
-    print!("{}: ", prompt);
-    io::stdout().flush().unwrap();
-    
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-    input.trim().to_string()
+    let prompt = format!("{}: ", prompt);
+    let mut rl = DefaultEditor::new()
+        .expect("Failed to create readline editor");
+    let readline = rl.readline(&prompt);
+
+    match readline {
+        Ok(line) => line.trim().to_string(),
+        Err(_) => String::new()
+    }
 }   
